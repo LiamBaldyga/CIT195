0255fe7c1b5d4ee43e669131b848176578589179
Liam <liambaldyga1@gmail.com>
2023-02-06 17:03:22 -0500

    Object model week 1

---
+++ C# Object Model/bookStoreWithCount/bookStoreWithCount/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace bookStoreWithCount
+
+{
+    class myStore
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.SetTrans();
+            book1.SetId(1);
+            book1.SetTitle("The Hobbit");
+            book1.SetAuthor("J.R.R. Tolkien");
+
+            Book book2 = new Book();
+            book2.SetTrans();
+            Console.WriteLine("Please enter the book ID: ");
+            book2.SetId(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Please enter the book title: ");
+            book2.SetTitle(Console.ReadLine());
+            Console.WriteLine("Please enter the book author: ");
+            book2.SetAuthor(Console.ReadLine());
+
+            Book book3 = new Book(3, "Dune", "Frank Herbert");
+            book3.SetTrans();
+
+            Console.WriteLine("Please enter the book ID: ");
+            int tempId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the book title: ");
+            string tempTitle = Console.ReadLine();
+            Console.WriteLine("Please enter the book author: ");
+            string tempAuthor = Console.ReadLine();
+            Book book4 = new Book(tempId, tempTitle, tempAuthor);
+            book4.SetTrans();
+
+
+            Console.WriteLine($"The store has {book1.GetTrans()} books");
+            displayBooks(book1);
+            displayBooks(book2);
+            displayBooks(book3);
+            displayBooks(book4);
+        }
+
+        static void displayBooks(Book book)
+        {
+            Console.WriteLine("Here is the book information");
+            Console.WriteLine($"Book Title: {book.GetTitle()}");
+            Console.WriteLine($"Book Author: {book.GetAuthor()}");
+            Console.WriteLine($"Book ID: {book.GetId()}");
+        }
+
+
+    }
+}
+
+class Book
+{
+    private int _Id;
+    private string _Title;
+    private string _Author;
+    private static int _transactions = 0;
+
+    public Book()
+    {
+        _Id = 0;
+        _Title = "";
+        _Author = "";
+    }
+
+    public Book(int id, string title, string author)
+    {
+        _Id = id;
+        _Title = title;
+        _Author = author;
+    }
+
+    public int GetTrans() {
+        return _transactions;
+    }
+
+    public void SetTrans()
+    {
+        ++_transactions;
+    }
+
+    public int GetId()
+    {
+        return _Id;
+    }
+
+    public void SetId(int id)
+    {
+        _Id = id;
+    }
+
+    public string GetTitle()
+    {
+        return _Title;
+    }
+
+    public void SetTitle(string title)
+    {
+        _Title = title;
+    }
+
+    public string GetAuthor()
+    {
+        return _Author;
+    }
+
+    public void SetAuthor(string author)
+    {
+        _Author = author;
+    }
+}
---
+++ C# Object Model/myProject/myProject/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace myProject
+{
+    class Game
+    {
+        private int _Id;
+        private string _Name;
+        private double _Price;
+
+        public Game()
+        {
+            _Id = 0;
+            _Name = "";
+            _Price = 0;
+        }
+
+        public Game(int id, string name, double price)
+        {
+            _Id = id;
+            _Name = name;
+            _Price = price;
+        }
+        public int GetId()
+        {
+            return _Id;
+        }
+
+        public void SetId(int id)
+        {
+            _Id = id;
+        }
+
+        public string GetName()
+        {
+            return _Name;
+        }
+
+        public void SetName(string name)
+        {
+            _Name = name;
+        }
+
+        public double GetPrice()
+        {
+            return _Price;
+        }
+
+        public void SetPrice(double price)
+        {
+            _Price = price;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1 = new Game();
+            game1.SetId(1);
+            game1.SetName("Minecraft");
+            game1.SetPrice(29.99);
+
+            Game game2 = new Game();
+            Console.Write("Enter the game ID: ");
+            game2.SetId(int.Parse(Console.ReadLine()));
+            Console.Write("Enter game name: ");
+            game2.SetName(Console.ReadLine());
+            Console.Write("Enter game price: $");
+            game2.SetPrice(double.Parse(Console.ReadLine()));
+
+            Game game3 = new Game(3, "Terraria", 10.00);
+
+            Console.Write("Enter game ID: ");
+            int tempId = int.Parse(Console.ReadLine());
+            Console.Write("Enter game name: ");
+            string tempName = Console.ReadLine();
+            Console.Write("Enter game price: $");
+            double tempPrice = double.Parse(Console.ReadLine());
+            Game game4 = new Game(tempId, tempName, tempPrice);
+
+            displayGames(game1);
+            displayGames(game2);
+            displayGames(game3);
+            displayGames(game4);
+        }
+
+        static void displayGames(Game game)
+        {
+            Console.WriteLine("----------Game Information----------");
+            Console.WriteLine($"Game Name: {game.GetName()}");
+            Console.WriteLine($"Game Price: ${game.GetPrice()}");
+            Console.WriteLine($"Game ID: {game.GetId()}");
+        }
+    }
+}
