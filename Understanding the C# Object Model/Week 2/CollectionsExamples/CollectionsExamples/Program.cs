8c6640cbb5ae0b0fedc8db5ea5134438f1b3b39e
Liam <liambaldyga1@gmail.com>
2023-03-13 18:51:42 -0400

    week 2

---
+++ Understanding the C# Object Model/Week 2/CollectionsExamples/CollectionsExamples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<int> queue = new Queue<int>();
+
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+        queue.Enqueue(40);
+        queue.Enqueue(50);
+
+        Console.WriteLine($"The queue {(queue.Contains(30) ? "Contains" : "Does not contain")} 30");
+
+        int item = queue.Dequeue();
+        Console.WriteLine("Removed item from queue: " + item);
+
+        Console.WriteLine("Queue count: " + queue.Count);
+
+        Console.WriteLine("Queue items:");
+        foreach (int i in queue)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine();
+
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        linkedList.AddLast("apple");
+        linkedList.AddLast("banana");
+        linkedList.AddLast("cherry");
+        linkedList.AddLast("date");
+        linkedList.AddLast("elderberry");
+
+        LinkedListNode<string> firstNode = linkedList.First;
+        Console.WriteLine("First node: " + firstNode.Value);
+
+        LinkedListNode<string> lastNode = linkedList.Last;
+        Console.WriteLine("Last node: " + lastNode.Value);
+
+        linkedList.AddAfter(linkedList.Find("cherry"), "durian");
+
+        linkedList.Remove("banana");
+
+        Console.WriteLine("Linked list count: " + linkedList.Count);
+
+        Console.WriteLine("Linked list items:");
+        foreach (string s in linkedList)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Understanding the C# Object Model/Week 2/LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter two doubles");
+        Console.Write("Double 1: ");
+        double d1 = double.Parse(Console.ReadLine());
+        Console.Write("Double 2: ");
+        double d2 = double.Parse(Console.ReadLine());
+
+        var sum = (double x, double y) => x + y;
+        Console.WriteLine($"Addition of {d1} and {d2} is {sum(d1, d2)}");
+
+        var product = (double x, double y) => x * y;
+        Console.WriteLine($"Multiplication of {d1} and {d2} is {product(d1, d2)}");
+
+        var smaller = (double x, double y) => x < y ? x : y;
+        Console.WriteLine($"Smaller number between {d1} and {d2} is {smaller(d1, d2)}");
+
+
+    }
+}
