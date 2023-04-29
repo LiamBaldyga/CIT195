2d8f63058ea3521e75464e32553485a070639825
Liam <liambaldyga1@gmail.com>
2023-04-29 16:50:17 -0400

    Database Version

---
+++ Graphical Applications Using C#/Week 3/Customers/AdventureWorkService/Models/CustomerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorkService.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
---
+++ Graphical Applications Using C#/Week 3/Customers/AdventureWorkService/Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdventureWorkService.Models
+{
+    [Table("Customer", Schema = "SalesLT")]
+    public class CustomerModel
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+    }
+}
